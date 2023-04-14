dea2f849b6995a69c4d978283524dff37bb6fe09
GolPooneh <119513155+GolPoooneh@users.noreply.github.com>
2023-04-14 23:36:10 +0430

    updated structure

--- Phase 0/src/main/java/controller/MatchMenuController.java
+++ Phase 0/src/main/java/controller/MatchMenuController.java
@@ -3,6 +3,9 @@ package controller;
 public class MatchMenuController {
     public void enterMapMenu() {
     }
+    
+    private boolean isEntryNotValid() {
+    }
 
     public String showPopularityFactors() {
     }
@@ -42,4 +45,65 @@ public class MatchMenuController {
 
     public String repair() {
     }
+
+    public String setTexture(int column, int row, String type){
+
+    }
+
+    public String setTexture(int column1, int row1, int column2, int row2, String type){
+
+    }
+
+    public String clear(int column, int row){
+
+    }
+
+    public String dropRock(int column, int row, Direction direction){
+
+    }
+
+    public String dropTree(int column, int row, String type){
+
+    }
+
+    public String dropBuilding(int column, int row, String type){
+
+    }
+
+    public String dropUnit(int column, int row, String type, int count){
+
+    }
+
+    public String selectUnit(int column, int row) {}
+    public String moveUnit(int column, int row) {
+    }
+
+    public String patrolUnit(int column1, int row1, int column2, int row2) {
+    }
+
+    public String stopPatrolUnit(int column, int row) {
+    }
+
+    public String setTroopState(int column, int row, String state) {
+    }
+
+    public String attackEnemy(int enemyColumn, int enemyRow) {
+    }
+
+    public String attack(int column, int row) {
+    }
+
+    public String pourOil(String direction) {
+    }
+
+    public String digTunnel(int column, int row) {
+    }
+
+    public String buildEquipment(String equipmentName) {
+    }
+
+    public String disbandUnit() {
+    }
+
+
 }
